c18c9cd6191300b476a03c9a638d1f8307de0349
Leonard <leonardglez12485@gmail.com>
2024-08-27 08:20:51 -0400

    Testing heroes array

--- test/baser-pruebas/08-imp-exp.test.js
+++ test/baser-pruebas/08-imp-exp.test.js
@@ -1,9 +1,9 @@
 import heroes from '../../data/heroes';
-import {getHeroeById} from '../../src/base-pruebas/08-imp-exp'
+import { getHeroeById, getHeroesByOwner } from '../../src/base-pruebas/08-imp-exp'
 
 
 describe('08-impo-exp', ()=>{
-    test('getHeroeById most return the heroe wiuth this Id',()=>{
+    test('getHeroeById most return the heroe with this Id',()=>{
        const id = 1;
        const heroe = getHeroeById(id);
        const heroeData = heroes.find( (heroe) => heroe.id === id );
@@ -25,4 +25,39 @@ describe('08-impo-exp', ()=>{
        const heroe = getHeroeById(id);
        expect(heroe).toBeFalsy();
     })
+    
+    test('getHeroesByOwner most return the heroe with this owner', ()=>{
+         const owner = 'DC';
+         const heroe = getHeroesByOwner(owner);
+         const heroeData = heroes.filter( (heroe) => heroe.owner === owner );
+         expect(heroe).toEqual(heroeData);
+    })
+
+    test('getHeroesByOwner most return an heroe by owner', ()=>{
+         const owner = 'DC';
+         const heroe = getHeroesByOwner(owner);
+         expect(heroe).toEqual([{
+               id: 1,
+               name: 'Batman',
+               owner: 'DC'
+         },
+         {
+               id: 3,
+               name: 'Superman',
+               owner: 'DC'
+         },
+         {
+               id: 4,
+               name: 'Flash',
+               owner: 'DC'
+         }
+      ])
+    })
+
+    test('getHeroesByOwner most return undefined if owner not existe', ()=>{
+      const owner = 'DH';
+         const heroe = getHeroesByOwner(owner);
+         expect(heroe).toEqual([]);
+    })
+
 })
